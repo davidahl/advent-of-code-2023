2d5c74de1799a71b7979db0cd2b37888f9e1cdac
David Ahl <david.ahl.axelsson@purplescout.se>
2023-12-10 12:19:09 +0100

    remove comments

--- day1/day1.go
+++ day1/day1.go
@@ -9,10 +9,6 @@ import (
 	"strings"
 )
 
-// func ReadLines()
-
-// var numbers [20]string = ["zero","0","one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight","8", "nine","9"]
-
 var numbers = map[string]string{
 	"zero":  "0",
 	"one":   "1",
